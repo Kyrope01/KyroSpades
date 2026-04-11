ddafc088ecdbd88a51d07ad8eda3d0f4c278a534
Kyrope <ky.the.rope@gmail.com>
2026-04-11 16:48:53 +0530

    Add files via upload

---
+++ src/font.h
@@ -0,0 +1,38 @@
+/*
+	Copyright (c) 2017-2020 ByteBit
+
+	This file is part of KyroSpades.
+
+	KyroSpades is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	KyroSpades is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with KyroSpades.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef FONT_H
+#define FONT_H
+
+enum font_type {
+	FONT_FIXEDSYS,
+	FONT_SMALLFNT,
+	FONT_FANTASY,
+};
+
+void font_init(void);
+void font_reset(void);
+float font_length(float h, char* text);
+void font_render(float x, float y, float h, char* text);
+void font_render_shadow(float x, float y, float h, char* text, float a);
+void font_centered(float x, float y, float h, char* text);
+void font_centered_shadow(float x, float y, float h, char* text, float a);
+void font_select(enum font_type type);
+
+#endif
